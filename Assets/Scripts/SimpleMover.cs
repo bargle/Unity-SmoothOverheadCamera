cfd83d7ddf05c8399d2440a832f1c8929208a3f9
David Koenig <yarnhammer@gmail.com>
2018-11-12 09:37:38 -0600

    Add simple mover script

---
+++ Assets/Scripts/SimpleMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SimpleMover : MonoBehaviour
+{
+	[SerializeField] private float m_speed = 10.0f;
+
+	private void Update()
+	{
+		float x = Input.GetAxis( "Horizontal" );
+		float z = Input.GetAxis( "Vertical" );
+
+		Vector3 position = transform.localPosition;
+		position.x += x * Time.deltaTime * m_speed;
+		position.z += z * Time.deltaTime * m_speed;
+
+		transform.localPosition = position;
+	}
+}
